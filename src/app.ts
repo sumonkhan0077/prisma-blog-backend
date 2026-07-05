0e8f89e5651f89e358e4d03573d8a3959f71e5e8
sumonkhan0077 <sumonkhanbd0077@gmail.com>
2026-07-05 11:41:32 +0600

    added config all and prisma setup

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,20 @@
+import cookieParser from "cookie-parser";
 import express, { Application, Request, Response } from "express";
+import config from "./config";
+import { join } from "node:path";
+import cors from "cors";
 
 
 const app : Application = express();
+
+app.use(cors({
+    origin: config.app_url,
+    credentials: true,
+}))
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(cookieParser())
 
 app.get("/",(req : Request, res : Response) => {
     res.send("Hello, World!");
--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,20 @@
 import app from "./app"
+import config from "./config";
+import { prisma } from "./lib/prisma";
 
-const PORT = process.env.PORT || 5000;
+
+const PORT = config.port ;
 
 async function main() {
     try {
+        // await prisma.$connect();
+        console.log('connected to the database successfully')
         app.listen(PORT, ( )=> {
             console.log(`server is running on port ${PORT}`)
         })
     } catch (error) {
         console.error("Error starting the server:", error);
+        // await prisma.$disconnect();
         process.exit(1);
     }
 }
